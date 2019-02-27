c1cfb542158d351113ef9e0d02c148bb3669b28b
Nik Katalnikov <nik.katalnikov@icloud.com>
2019-02-27 22:14:14 +0200

    chore: add jar serving option

--- deployment/bigkube.sh
+++ deployment/bigkube.sh
@@ -6,8 +6,14 @@ function wait() {
     done
 }
 
+function serve_jar_directory () {
+    docker rm nginx-jar-server -f
+    local PARENT_DIR=$(cd ../ && pwd)
+    docker run --name nginx-jar-server -v ${PARENT_DIR}/target/scala-2.11:/usr/share/nginx/html:ro -d -p 8080:80 nginx
+}
+
 function init_spark_operator() {
-    kubectl create clusterrolebinding minikube-cluster-admin-binding --clusterrole=cluster-admin --user=minikube
+    kubectl create clusterrolebinding default --clusterrole=edit --serviceaccount=default:default --namespace=default
     helm init &&
     helm install incubator/sparkoperator --namespace spark-operator --set enableWebhook=true
 }
@@ -32,6 +38,9 @@ function delete() {
 cd "$(dirname "$0")"
 while [ $# -gt 0 ]; do
     case "$1" in
+        --serve-jar)
+            serve_jar_directory
+            ;;
         --spark-init)
             init_spark_operator
             ;;
