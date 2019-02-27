ef1bdc04114bdd9fe2789188d3fce96fd8172938
Nik Katalnikov <nik.katalnikov@icloud.com>
2019-02-27 22:15:41 +0200

    chore: fix presto init

--- src/it/scala/it_tests/utils/PrestoService.scala
+++ src/it/scala/it_tests/utils/PrestoService.scala
@@ -1,9 +1,11 @@
 package it_tests.utils
 
+import com.typesafe.config.ConfigFactory
 import slick.jdbc.JdbcProfile
 import slick.basic.DatabaseConfig
 
 object PrestoService {
-  val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig("prestodb")
+  private val config = ConfigFactory.load("it")
+  val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig("prestodb", config)
   val db: JdbcProfile#Backend#Database = dbConfig.db
 }
