e852e4c8b8e1623529b26ae7ef3be0ed804019c6
Nik Katalnikov <nik.katalnikov@icloud.com>
2019-03-06 19:09:45 +0200

    it: refactor, add presto JDBC service, add simple test, update configs

--- src/it/scala/it_tests/KafkaTest.scala
+++ src/it/scala/it_tests/KafkaTest.scala
@@ -6,16 +6,12 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
-import scala.util.{Failure, Success}
 import scala.collection.JavaConverters._
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 
 class KafkaTest extends FunSuite with BeforeAndAfterAll with Matchers {
-  import scala.concurrent.ExecutionContext.Implicits.global
-
   private val config = ConfigFactory.load("it")
   private val localKafkaUrl = config.getString("kafka.url")
+  private val tableName = config.getString("hive.tableName")
 
   private val topics = config
     .getStringList("kafka.topics")
@@ -43,38 +39,26 @@ class KafkaTest extends FunSuite with BeforeAndAfterAll with Matchers {
     } catch {
       case _: Throwable => sparkController.cleanUpSparkTestDeployment()
     }
-  }
 
-  test("it reads data from Kafka topics and stores it to hdfs") {
-    val recordsNumber = 100
-    val records = FakeMsgGenerator.generateNFakeMsgs(recordsNumber)
-    val fs = topics
+    val records = FakeMsgGenerator.generateNFakeMsgs(100)
+    topics
       .map(t => (t, consumer.AvroHelper.serializeMsg(records)))
       .map {
         case (t, array) => new ProducerRecord[String, Array[Byte]](t, array)
       }
-      .map(x => Future(producer.send(x)))
+      .foreach(x => producer.send(x))
 
-    val seqF = Future.sequence(fs)
-
-    seqF onComplete {
-      case Success(v) => v
-      case Failure(e) => throw new RuntimeException(s"Producer error $e")
-    }
-
-    Await.result(seqF, Duration.Inf)
     producer.close()
 
-    import PrestoService.dbConfig.profile.api._
-    lazy val verifyQ = sqlu"""select count(*) from test"""
-    val qF = PrestoService.db.run(verifyQ)
+    // let Spark Streaming fetch latest from Kafka msg and store it
+    Thread.sleep(60000)
+  }
 
-    qF onComplete {
-      case Success(v) => assert(v == recordsNumber)
-      case Failure(e) => throw new RuntimeException(s"Presto query error $e")
-    }
+  test("it reads data from Kafka topics and stores it to hdfs") {
+    val singleRowQ = s"SELECT COUNT(*) FROM $tableName"
+    val count = PrestoService.execStatement(singleRowQ, _.getInt(1)).head
 
-    Await.result(qF, Duration.Inf)
+    count should be > 0
   }
 
   override def afterAll(): Unit = {
--- src/it/scala/it_tests/utils/PrestoService.scala
+++ src/it/scala/it_tests/utils/PrestoService.scala
@@ -1,11 +1,31 @@
 package it_tests.utils
 
+import java.sql.{Connection, DriverManager, ResultSet}
+import java.util.Properties
+
 import com.typesafe.config.ConfigFactory
-import slick.jdbc.JdbcProfile
-import slick.basic.DatabaseConfig
 
 object PrestoService {
   private val config = ConfigFactory.load("it")
-  val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig("prestodb", config)
-  val db: JdbcProfile#Backend#Database = dbConfig.db
+
+  private val properties = new Properties()
+  properties.setProperty("user", config.getString("prestodb.db.user"))
+  properties.setProperty("password", config.getString("prestodb.db.password"))
+
+  private val url = config.getString("prestodb.db.url")
+
+  private val connection: Connection = DriverManager.getConnection(url, properties)
+
+  def execStatement[T](sql: String, f: ResultSet => T): Stream[T] = {
+    val statement = connection.createStatement()
+    val rs = statement.executeQuery(sql)
+
+    val result = Stream
+      .continually()
+      .takeWhile(_ => rs.next)
+      .map(_ => f(rs))
+
+    statement.close()
+    result
+  }
 }
