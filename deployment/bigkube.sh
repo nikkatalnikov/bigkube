9c56c266d8b8dabd9f717d783cc51ad3eae59e2f
mhr <grigor60@gmail.com>
2019-03-22 15:19:12 +0200

    Create test ms sql DB. With out installing sqlcmd.

--- deployment/bigkube.sh
+++ deployment/bigkube.sh
@@ -29,14 +29,14 @@ function create() {
     kubectl create secret generic mssql-user --from-literal=user=sa
     kubectl create secret generic mssql-password --from-literal=password=YOUR_PASSWORD_123_abcd
     kubectl create -f mssql.yaml && wait
-    local MSSQL_NODE_PORT=$(kubectl get svc mssql -o=jsonpath='{.spec.ports[?(@.port==1433)].nodePort}')
-    sqlcmd -S $(minikube ip),MSSQL_NODE_PORT -U sa -P YOUR_PASSWORD_123_abcd -i create_db.sql
+    kubectl create -f mssql-init-command.yaml && wait
+    kubectl logs -f mssql-init-command
     kubectl create -f kafka.yaml && wait
     kubectl create -f schema-registry.yaml && wait
-    kubectl create configmap hive-env --from-env-file hive.env --dry-run -o yaml | kubectl apply -f -
-    kubectl create -f hdfs.yaml && wait
-    kubectl create -f metastore.yaml && wait
-    kubectl create -f presto.yaml && wait
+#    kubectl create configmap hive-env --from-env-file hive.env --dry-run -o yaml | kubectl apply -f -
+#    kubectl create -f hdfs.yaml && wait
+#    kubectl create -f metastore.yaml && wait
+#    kubectl create -f presto.yaml && wait
 }
 
 function delete() {
